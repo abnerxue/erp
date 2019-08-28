73a5d6b1f8f9e178ddd7d69252f3a7f553e4a844
薛松田 <498572743@qq.com>
2019-08-28 16:07:31 +0800

    456

--- src/views/index/mainIndex.vue
+++ src/views/index/mainIndex.vue
@@ -34,8 +34,8 @@
           <p class="title"><i class="fa fa-server"></i>版本信息</p>
           <div class="table">
             <p><span class="tit">当前版本</span>常孝天下后台管理v1.0.0</p>
-            <p><span class="tit">基于框架</span>vue2.0 + element-ui</p>
-            <p><span class="tit">主要特色</span>单页面 / 响应式 / 无限级菜单 / 权限管理 / 极简</p>
+            <p><span class="tit">基于框架</span>vue2.0 + element</p>
+            <p><span class="tit">主要特色</span> 响应式 / 无限级菜单 / 权限管理 / 极简</p>
            <!--  <p><span class="tit">获取渠道</span>
                 <span class="gitbox">
                   <a target="_blank" href="https://github.com/Nirongxu/vue-xuAdmin.git"><el-button type="primary" icon="el-icon-download" size="small">github</el-button></a>
